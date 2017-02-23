3e32605f13a252fdafe754e42f0b91182dcf6a3e
skysunshine99 <skysunshine99@126.com>
2017-02-23 09:59:24 +0800

    remove TTTransfer

--- index.js
+++ index.js
@@ -2,8 +2,6 @@
  * @author rubyisapm
  */
 import LLTransfer from './src/LLTransfer/LLTransfer.vue';
-import TTTransfer from './src/TTTransfer/TTTransfer.vue';
 export default {
-    LLTransfer,
-    TTTransfer
+    LLTransfer
 }
